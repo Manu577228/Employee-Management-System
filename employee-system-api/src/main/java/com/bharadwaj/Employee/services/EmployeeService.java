8b34fca3d11c91c62859192b8a4b63a1b6520ea0
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2023-08-15 21:31:26 +0530

    First commit

---
+++ employee-system-api/src/main/java/com/bharadwaj/Employee/services/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.bharadwaj.Employee.services;
+
+import com.bharadwaj.Employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
---
+++ employee-system-ui/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AddEmployee from "./components/AddEmployee";
+import Navbar from "./components/Navbar";
+import EmployeeList from "./components/EmployeeList";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route index element={<EmployeeList />} />
+          <Route path="/" element={<EmployeeList />} />
+          <Route path="/employeeList" element={<EmployeeList />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
